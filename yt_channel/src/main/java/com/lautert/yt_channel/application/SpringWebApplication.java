e9f19e43f79a8a4d2e6ee03126b68128f175915c
Guilherme Lautert <lautert250@gmail.com>
2022-02-10 22:19:48 -0300

    I had to go back because @Test fails, not finding the beans.

--- yt_channel/src/main/java/com/lautert/yt_channel/application/SpringWebApplication.java
+++ yt_channel/src/main/java/com/lautert/yt_channel/application/SpringWebApplication.java
@@ -7,9 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages =
+{
+    "com.lautert.yt_channel"
+})
+@ComponentScan(basePackages =
 {
     "com.lautert.yt_channel"
 })
